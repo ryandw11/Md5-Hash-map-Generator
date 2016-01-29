d1d972e296284e65cef6667fd15d3680a38d9078
ryandw11 <ryandw02@gmail.com>
2016-01-28 21:09:27 -0600

    Create JavaWindow.java

---
+++ src/Package/JavaWindow.java
@@ -0,0 +1,15 @@
+import javax.swing.JFrame;
+ 
+public class JavaWindow{
+ 
+public static void main(String[] args) {
+ 
+// TODO Auto-generated method stub
+ 
+MyWindow window = new MyWindow("Hashmap"); // New window named Hashmap
+ 
+window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+ 
+}//End main
+ 
+}//End class
